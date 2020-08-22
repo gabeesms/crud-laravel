ab4308540cab12b2ae50663cacfb73ffef0c13b3
Gabrielle <gabriellemacieel@gmail.com>
2020-08-22 14:03:41 -0400

    alterar router
    router resource

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('pessoas', 'PessoaController@index');
-Route::post('pessoas', 'PessoaController@store');
-Route::get('pessoas/create', 'PessoaController@create');
-Route::get('pessoas/{pessoa}/edit', 'PessoaController@edit');
-Route::get('pessoas/{pessoa}', 'PessoaController@show');
-Route::put('pessoas/{pessoa}', 'PessoaController@update');
-Route::delete('pessoas/{pessoa}', 'PessoaController@destroy');
+// Route::get('pessoas', 'PessoaController@index');
+// Route::post('pessoas', 'PessoaController@store');
+// Route::get('pessoas/create', 'PessoaController@create');
+// Route::get('pessoas/{pessoa}/edit', 'PessoaController@edit');
+// Route::get('pessoas/{pessoa}', 'PessoaController@show');
+// Route::put('pessoas/{pessoa}', 'PessoaController@update');
+// Route::delete('pessoas/{pessoa}', 'PessoaController@destroy');
+
+Route::resource('pessoas', 'PessoaController');
